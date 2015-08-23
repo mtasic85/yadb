394373ff674f740e9325272b9cdcba91f950c9d5
Marko Tasic <mtasic85@gmail.com>
2015-08-23 15:03:13 +0200

    store: transactions

--- store/transaction.py
+++ store/transaction.py
@@ -1,13 +1,19 @@
 __all__ = ['Transaction']
 
+import threading
+
 class Transaction(object):
     def __init__(self, store):
         self.store = store
         self.read_log = []
         self.write_log = []
-    
+
     def __enter__(self):
+        # thread local transaction
+        local = threading.local()
+        local._yadb_store_transaction = self
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        local._yadb_store_transaction = None
         return False
